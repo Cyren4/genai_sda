0e13c3d121e3dec6d615dad19960cc1723274f50
Yoav <yoavcohen96@gmail.com>
2025-03-28 17:44:32 +0100

    Listes

--- src/data_loading/search_recipes.py
+++ src/data_loading/search_recipes.py
@@ -1,7 +1,56 @@
-mots_cles_nourriture = [
-    "Poulet", "Quiche", "Riz", "Pâtes", "Fromage", "Poisson", "Viande", "Légumes", 
-    "Fruits", "Pain", "Soupe", "Gâteau", "Salade", "Chocolat", "Yaourt", "Crêpe", 
-    "Omelette", "Saumon", "Pizza", "Curry"
+# Catégorie : Entrées
+entrees = [
+    "Salade", "Soupe", "Quiche", "Bruschetta", "Tapenade", "Velouté", "Carpaccio", "Terrine", 
+    "Gazpacho", "Tartare", "Omelette", "Houmous", "Caviar d’aubergine", "Samossa", "Accras", 
+    "Rillettes", "Blinis", "Mousse salée", "Gougères", "Tzatziki"
 ]
 
-print(mots_cles_nourriture)
+# Catégorie : Plats principaux
+plats_principaux = [
+    "Poulet rôti", "Bœuf bourguignon", "Lasagnes", "Couscous", "Paella", "Ratatouille", "Blanquette de veau", 
+    "Coq au vin", "Steak frites", "Curry de légumes", "Tajine", "Risotto", "Chili con carne", "Quiche lorraine", 
+    "Tartiflette", "Hachis parmentier", "Boulettes de viande", "Rôti de porc", "Gratin dauphinois", "Filet de saumon"
+]
+
+# Catégorie : Desserts
+desserts = [
+    "Gâteau au chocolat", "Tarte aux pommes", "Mousse au chocolat", "Crème brûlée", "Panna cotta", "Tiramisu", 
+    "Clafoutis", "Cheesecake", "Île flottante", "Crêpes", "Beignets", "Macarons", "Brownie", "Éclair au chocolat", 
+    "Charlotte aux fraises", "Flan pâtissier", "Millefeuille", "Galette des rois", "Baba au rhum", "Fondant au chocolat"
+]
+
+# Catégorie : Accompagnements
+accompagnements = [
+    "Purée de pommes de terre", "Riz pilaf", "Frites", "Haricots verts", "Ratatouille", "Semoule", 
+    "Pommes dauphines", "Risotto", "Gratin de légumes", "Polenta", "Taboulé", "Salade verte", 
+    "Flan de courgettes", "Poêlée de champignons", "Tian de légumes", "Boulgour", "Épinards sautés", 
+    "Coleslaw", "Lentilles", "Pommes de terre sautées"
+]
+
+# Catégorie : Ingrédients de base
+ingredients = [
+    "Farine", "Sucre", "Sel", "Poivre", "Beurre", "Huile d'olive", "Lait", "Œufs", "Levure", "Crème fraîche",
+    "Miel", "Vinaigre", "Tomates", "Oignons", "Ail", "Herbes de Provence", "Paprika", "Piment", "Moutarde", "Bouillon"
+]
+
+# Catégorie : Boissons
+boissons = [
+    "Eau", "Café", "Thé", "Jus d’orange", "Jus de pomme", "Limonade", "Soda", "Vin rouge", "Vin blanc", "Bière",
+    "Cocktail", "Smoothie", "Milkshake", "Chocolat chaud", "Infusion", "Cappuccino", "Mojito", "Spritz", "Sangria", "Cidre"
+]
+
+# Catégorie : Épices et condiments
+epices_condiments = [
+    "Curry", "Curcuma", "Cumin", "Cannelle", "Gingembre", "Piment d'Espelette", "Paprika", "Clous de girofle", 
+    "Noix de muscade", "Cardamome", "Anis étoilé", "Estragon", "Basilic", "Coriandre", "Thym", "Romarin", 
+    "Persil", "Origan", "Fenouil", "Laurier"
+]
+
+# Affichage des listes
+print("Entrées :", entrees)
+print("Plats principaux :", plats_principaux)
+print("Desserts :", desserts)
+print("Accompagnements :", accompagnements)
+print("Ingrédients de base :", ingredients)
+print("Boissons :", boissons)
+print("Épices et condiments :", epices_condiments)
