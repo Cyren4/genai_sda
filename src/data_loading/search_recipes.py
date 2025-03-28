91384640cbca248e3afa287593378984ebfd38a8
Yoav <yoavcohen96@gmail.com>
2025-03-28 17:40:59 +0100

    Data

---
+++ src/data_loading/search_recipes.py
@@ -0,0 +1,7 @@
+mots_cles_nourriture = [
+    "Poulet", "Quiche", "Riz", "Pâtes", "Fromage", "Poisson", "Viande", "Légumes", 
+    "Fruits", "Pain", "Soupe", "Gâteau", "Salade", "Chocolat", "Yaourt", "Crêpe", 
+    "Omelette", "Saumon", "Pizza", "Curry"
+]
+
+print(mots_cles_nourriture)
